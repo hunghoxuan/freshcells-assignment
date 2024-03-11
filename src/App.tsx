712fee813e1fa7e486b250107c05c842729c5630
hunghx <hunghx@vmodev.com>
2024-03-11 16:42:57 +0100

    Add Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,25 @@ import ProtectedRoute from './components/layouts/ProtectedRoute'
 import Layout from './components/layouts/Layout'
 import { ROUTES } from './config/constants'
 import ErrorBoundary from './contexts/ErrorBoundary'
+import { Suspense } from 'react'
 
 export const App = () => (
-  <Layout>
-    <ErrorBoundary>
-      <Routes>
-        <Route element={<ProtectedRoute />}>
-          <Route path={ROUTES.ACCOUNT} element={<AccountPage />} />
-        </Route>
-        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-        <Route
-          path={ROUTES.NOT_FOUND}
-          element={<Navigate to={ROUTES.LOGIN} replace />}
-        />
-      </Routes>
-    </ErrorBoundary>
-  </Layout>
+  <Suspense fallback={<div>Loading...</div>}>
+    <Layout>
+      <ErrorBoundary>
+        <Routes>
+          <Route element={<ProtectedRoute />}>
+            <Route path={ROUTES.ACCOUNT} element={<AccountPage />} />
+          </Route>
+          <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+          <Route
+            path={ROUTES.NOT_FOUND}
+            element={<Navigate to={ROUTES.LOGIN} replace />}
+          />
+        </Routes>
+      </ErrorBoundary>
+    </Layout>
+  </Suspense>
 )
 
 export default App
